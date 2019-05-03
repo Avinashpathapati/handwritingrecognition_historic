1dad24686305a6269137ac5b29a7f3bb844a21ac
anpenta <anpenta01@gmail.com>
2019-05-03 15:16:28 +0200

    add functionality for scaling/translation/rotation

--- classifier/test.py
+++ classifier/test.py
@@ -1,9 +1,14 @@
 # Test module
 # Module to test the classifier modules.
 
-from utility import load_data, plot
+from utility import load_data, plot, rotate, translate, scale
 
 
 data = load_data("/home/anpenta/Desktop/character-classifier/data/monkbrill-jpg/monkbrill2")
-for image, label in zip(data["images"], data["labels"]):
-  plot(image, label)
+
+plot(data["images"][0], "before")
+plot(scale(data["images"][0]), "after")
+
+
+# for image, label in zip(data["images"], data["labels"]):
+  #plot(image, label)
--- classifier/utility.py
+++ classifier/utility.py
@@ -4,6 +4,8 @@
 import cv2 as cv
 import pandas as pd
 import os
+import random
+import numpy as np
 
 
 def plot(image, name):
@@ -14,8 +16,26 @@ def plot(image, name):
 def read_image(path):
   return cv.imread(path, cv.IMREAD_UNCHANGED)
 
-def resize(image):
-  return cv.resize(image, (256, 256))
+def scale(image):
+  (height, width) = image.shape[:2] 
+  scale_type = random.choice([cv.INTER_CUBIC, cv.INTER_AREA])
+  height = int(1.5 * height) if scale_type == cv.INTER_AREA else int(height / 1.5)
+  width = int(1.5 * width) if scale_type == cv.INTER_AREA else int(width / 1.5)
+  print(scale_type)
+  return cv.resize(image, (width, height), interpolation = scale_type) 
+
+def rotate(image):
+  (rows, columns) = image.shape[:2]
+  rotation_degree = random.choice([-10, -5, 5, 10])
+  matrix = cv.getRotationMatrix2D((columns / 2, rows / 2), rotation_degree, 1) 
+  return cv.warpAffine(image, matrix, (columns, rows), borderValue = 255) 
+
+def translate(image):
+  (rows, columns) = image.shape[:2] 
+  horizontal_shift = random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+  vertical_shift = random.choice([-4, -3, -2, -1, 0, 1, 2, 3, 4])
+  matrix = np.float32([[1, 0, horizontal_shift], [0, 1, vertical_shift]]) 
+  return cv.warpAffine(image, matrix, (columns, rows), borderValue = 255)
 
 def load_data(path):
   print("loading images...")
