6f51de3076f942ddc7362773f1bdc699dc46620c
anpenta <anpenta01@gmail.com>
2019-05-04 19:57:11 +0200

    change cnn to a variant of alexnet

--- classifier/cnn.py
+++ classifier/cnn.py
@@ -9,6 +9,7 @@ from keras.constraints import maxnorm
 from keras.layers.core import Flatten
 from keras.layers.core import Dropout
 from keras.layers.core import Dense
+from keras.layers.core import BatchNormalization
 from keras import backend as K
 
 
@@ -21,13 +22,33 @@ def build_cnn(height, width, depth, classes):
 
   # Build the model
   model = Sequential()
-  model.add(Conv2D(32, (3, 3), input_shape=input_shape, padding="same", activation="relu", kernel_constraint=maxnorm(3)))
-  model.add(Dropout(0.2))
-  model.add(Conv2D(32, (3, 3), activation="relu", padding="same", kernel_constraint=maxnorm(3)))
+  
+  model.add(Conv2D(32, (11, 11), input_shape=input_shape, padding="same"))
+  model.add(BatchNormalization())
+  model.add(Activation("relu"))
   model.add(MaxPooling2D(pool_size=(2, 2)))
+  
+  model.add(Conv2D(64, (7, 7), padding="same"))
+  model.add(BatchNormalization())
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+
+  model.add(Conv2D(128, (5, 5), padding="same"))
+  model.add(BatchNormalization())
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  
+  model.add(Conv2D(256, (3, 3), padding="same"))
+  model.add(BatchNormalization())
+  model.add(Activation("relu"))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+
   model.add(Flatten())
-  model.add(Dense(512, activation="relu", kernel_constraint=maxnorm(3)))
+  model.add(Dense(512))
+  model.add(BatchNormalization())
+  model.add(Activation("relu"))
   model.add(Dropout(0.5))
+
   model.add(Dense(classes, activation="softmax"))
 
   return model
